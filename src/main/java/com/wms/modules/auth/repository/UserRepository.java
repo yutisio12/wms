dade88e8cdb9fb3e0d9502e8c812dd69c2999f79
yutisio12 <yutisiogamings@gmail.com>
2026-05-05 21:54:26 +0700

    BE:
    - create UserRepository def

---
+++ src/main/java/com/wms/modules/auth/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.wms.modules.auth.repository;
+
+import com.wms.modules.auth.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+  Optional<User> findByUsername(String username);
+}
