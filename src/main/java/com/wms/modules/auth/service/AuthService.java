ae3127260f56e7312bec64133af58f0bcde24321
yutisio12 <yutisiogamings@gmail.com>
2026-05-05 21:57:30 +0700

    BE:
    - activate authservice due to userrepo has been created

--- src/main/java/com/wms/modules/auth/service/AuthService.java
+++ src/main/java/com/wms/modules/auth/service/AuthService.java
@@ -1,31 +1,29 @@
 package com.wms.modules.auth.service;
 
 import com.wms.modules.auth.domain.User;
-// import com.wms.modules.auth.repository.UserRepository;
+import com.wms.modules.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-  // private final UserRepository userRepo;
+  private final UserRepository userRepo;
 
   public String login(String username, String password) {
 
-    // User user = userRepo.findByUsername(username)
-    // .orElseThrow(() -> new RuntimeException("User not found"));
+    User user = userRepo.findByUsername(username)
+        .orElseThrow(() -> new RuntimeException("User not found"));
 
-    // if (!user.getPassword().equals(password)) {
-    // throw new RuntimeException("Wrong password");
-    // }
+    if (!user.getPassword().equals(password)) {
+      throw new RuntimeException("Wrong password");
+    }
 
-    // if (user.getTwoFaSecret() != null) {
-    // return "2FA_REQUIRED";
-    // }
+    if (user.getTwoFaSecret() != null) {
+      return "2FA_REQUIRED";
+    }
 
-    // return user.getId().toString();
-    return null;
+    return user.getId().toString();
+    // return null;
   }
 }
